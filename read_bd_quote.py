fa3bacbf1929e801b792bf51c3b9d1925bb7e865
Aleksey Manin <Alkor135@gmail.com>
2025-06-30 00:19:33 +0300

    update read_bd_quote.py

--- read_bd_quote.py
+++ read_bd_quote.py
@@ -2,16 +2,27 @@ import pandas as pd
 from pathlib import Path
 import sqlite3
 
-def read_sqlite_db(db_path: Path) -> pd.DataFrame:
+def read_db_quote(db_path_quote: Path) -> pd.DataFrame:
     """
     Читает SQLite базу данных и возвращает DataFrame.
     """
-    with sqlite3.connect(db_path) as conn:
+    with sqlite3.connect(db_path_quote) as conn:
         return pd.read_sql_query("SELECT * FROM Futures", conn)
-
-def main(path_db: Path) -> None:
     
-    df = read_sqlite_db(path_db)
+def read_db_news(db_path_news, date_max, date_min) -> pd.DataFrame:
+    """
+    Читает SQLite базу данных и возвращает DataFrame с новостями между date_min и date_max.
+    """
+    with sqlite3.connect(db_path_news) as conn:
+        query = """
+            SELECT * FROM news
+            WHERE date > ? AND date < ?
+        """
+        return pd.read_sql_query(query, conn, params=(date_min, date_max))
+
+def main(path_db_quote: Path, path_db_news: Path) -> None:
+    
+    df = read_db_quote(path_db_quote)
     df['TRADEDATE'] = pd.to_datetime(df['TRADEDATE'])  # Преобразуем даты в datetime
 
     # Перебираем пары строк начиная с последней
@@ -29,12 +40,19 @@ def main(path_db: Path) -> None:
         # Добавляем время 18:45:00 к датам
         date_max = date_max.replace(hour=18, minute=45, second=0)
         date_min = date_min.replace(hour=18, minute=45, second=0)
+
+        date_max_str = date_max.strftime('%Y-%m-%d %H:%M:%S')
+        date_min_str = date_min.strftime('%Y-%m-%d %H:%M:%S')
         
         print(f"Пара строк: {i}, {i-1}")
         print(f"{file_name} Дата max: {date_max}, Дата min: {date_min}")
-        # break
+        # df_news = read_db_news(path_db_news, date_max, date_min)
+        df_news = read_db_news(path_db_news, date_max_str, date_min_str)
+        print(df_news)
+        break
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
-    path_db = Path(fr'c:\Users\Alkor\gd\data_quote_db\RTS_day_2014.db')
+    path_db_quote = Path(fr'c:\Users\Alkor\gd\data_quote_db\RTS_day_2014.db')
+    path_db_news = Path(fr'C:\Users\Alkor\gd\data_rss_db\rss_news_investing.db')
 
-    main(path_db)
+    main(path_db_quote, path_db_news)
